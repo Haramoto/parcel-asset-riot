76f0ddbd96e5a2573e698802564cd3da5bbda199
原本欽朗 <harapon81@genponkinakiranoMacBook-Air.local>
2018-03-05 19:13:22 +0900

    init

---
+++ riotAssetType.js
@@ -0,0 +1,35 @@
+const {Asset} = require('parcel-bundler');
+const JSAssetLib = require('parcel-bundler/lib/assets/JSAsset')
+const JSAssetSrc = require('parcel-bundler/src/assets/JSAsset')
+
+const JSAsset = parseInt(process.versions.node, 10) < 8 ? JSAssetLib : JSAssetSrc
+const riot = require("riot-compiler")
+
+class RiotAsset extends JSAsset {
+  async parse(tagCode) {
+    let config = this.package.riot ||
+        (await this.getConfig(['.riotrc', '.riotrc.js'])) ||
+        {}
+
+    let transpiled = riot.compile(
+      tagCode,
+      Object.assign({sourcemap: this.options.sourceMap}, config),
+      this.relativeName
+    )
+
+    let code = this.options.sourceMap ? transpiled.code : transpiled
+
+    //    if (transpiled.map) {
+    //      this.sourceMap = transpiled.map.toJSON();
+    //      this.sourceMap.sources = [this.relativeName];
+    //      this.sourceMap.sourcesContent = [this.contents];
+    //    }
+
+    // Overwrite contents
+    this.contents = code;
+
+    return await JSAsset.prototype.parse.call(this, code);
+  }
+}
+
+module.exports = RiotAsset
